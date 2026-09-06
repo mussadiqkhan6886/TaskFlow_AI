4424608e30afd80da183aadb8a43f31a9ea272e9
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-09-06 19:38:40 +0500

    delete note test done

---
+++ frontend/__tests__/components/DeleteNote.test.tsx
@@ -0,0 +1,52 @@
+import DeleteNote from "@/components/DeleteNote"
+import { renderWithQuery } from "@/lib/helpers/renderWithQuery"
+import { deleteNote } from "@/server/note"
+import { screen, waitFor } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
+
+vi.mock("@/server/note", () => ({
+    deleteNote: vi.fn()
+}))
+
+const refreshMock = vi.fn()
+vi.mock("next/navigation", () => ({
+    useRouter: () => ({
+        refresh: refreshMock
+    })
+}))
+
+describe("delete note", () => {
+
+    beforeEach(() => {
+        vi.clearAllMocks()
+    })
+    
+    it("will delete note successfully", async () => {
+        const user = userEvent.setup()
+        const mockedFun = vi.mocked(deleteNote)
+        mockedFun.mockResolvedValue(undefined)
+        renderWithQuery(<DeleteNote id={"123"} />)
+
+        await user.click(screen.getByRole("button", {name: /Delete/i}))
+
+        await waitFor(() => {
+            expect(mockedFun).toHaveBeenCalledWith("123")
+            expect(refreshMock).toHaveBeenCalledTimes(1)
+        })
+
+    })
+
+    it("will not delete note and gives Error", async () => {
+        const user = userEvent.setup()
+        const mockedFun = vi.mocked(deleteNote)
+        mockedFun.mockRejectedValue(new Error("Something went wrong"))
+        renderWithQuery(<DeleteNote id={"123"} />)
+        const alertMock = vi.spyOn(window, "alert").mockImplementation(() => {});
+        await user.click(screen.getByRole("button", {name: /Delete/i}))
+        await waitFor(() => {
+            expect(alertMock).toHaveBeenCalledWith("Something went wrong");
+        });
+        alertMock.mockRestore(); 
+    })      
+})
