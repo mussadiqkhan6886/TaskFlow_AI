2b852b921c7684e630eb8013fe2badc55db141d8
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-09-11 01:08:14 +0500

    note controllerr

--- backend/__tests__/controllers/noteContollers.test.ts
+++ backend/__tests__/controllers/noteContollers.test.ts
@@ -12,3 +12,48 @@ vi.mock("../config/connectRedis", () => ({
     redis: vi.fn()
 }))
 
+describe("note controller", () => {
+
+    it("will get all notes", () => {
+
+    })
+
+    it("will get single note", () => {
+        
+    })
+
+    it("will not get single note and send status of 400", () => {
+
+    })
+
+    it("will create new note successfully ", () => {
+        
+    })
+
+    it("will not create new note and send status of 400", () => {
+        
+    })
+
+    it("will update current note successfully", () => {
+        
+    })
+    
+    it("will not update current note and send status of 400", () => {
+
+    })
+    
+    it("will not update current note if note is not found and send status of 404", () => {
+
+    })
+
+    it("will delete note successfully", () => {
+        
+    })
+
+    it("will not delete note if id is not given and  send status of 400", () => {
+
+    })
+    it("will not delete note if note is not found and send status of 404", () => {
+
+    })
+})
