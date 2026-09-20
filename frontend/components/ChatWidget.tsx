56a39eae12f784e12c5f813749e8368cbdb24c4a
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-09-20 23:12:04 +0500

    starting chatuser

--- frontend/components/ChatWidget.tsx
+++ frontend/components/ChatWidget.tsx
@@ -1,19 +1,28 @@
-import { getMe } from '@/server/user'
-import React from 'react'
-import ChatStaff from './ChatStaff'
-import ChatUser from './ChatUser'
+import { getMe } from "@/server/user";
+import ChatStaff from "./ChatStaff";
+import ChatUser from "./ChatUser";
+
 
 const ChatWidget = async () => {
-    const me = await getMe()
-    return  (
-        <section>
-            {me.role === "Employee" ? <ChatUser /> : 
-            <div>
-                <ChatUser/>
-                <ChatStaff/>
-            </div>}
-        </section>
-    )
-}
 
-export default ChatWidget
+    const me = await getMe();
+
+    const isStaff =
+        me.role === "Admin" ||
+        me.role === "Manager";
+
+    return (
+        <section>
+
+            <ChatUser />
+
+            {isStaff && (
+                <ChatStaff />
+            )}
+
+        </section>
+    );
+};
+
+
+export default ChatWidget;
