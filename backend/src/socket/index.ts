cc9b40b1fb4bde7ada201c62053dfb7867b3a92c
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-09-20 19:43:47 +0500

    creating chat app for group of admin and manager and also group chat for all users

--- backend/src/socket/index.ts
+++ backend/src/socket/index.ts
@@ -3,10 +3,10 @@ import type { Server, Socket } from "socket.io";
 export const socketConfig = (io: Server) => {
     io.on("connection", (socket: Socket) => {
 
-    console.log("Connected:", socket.id);
+        console.log("Connected:", socket.id);
 
 
-    socket.on("disconnect", () => {
+        socket.on("disconnect", () => {
         console.log("Disconnected:", socket.id);
     });
 
